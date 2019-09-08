12c2592a830223f32805f53a658b0c95346470b2
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-08 01:06:57 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+require 'pry'
 class BankAccount
-
+  attr_accessor :name, :balance, :status
+  
+  def initialize(name,balance = 1000, status = 'open')
+    @name = name
+    @balance = balance
+    @status = status 
+    @name.freeze
+    #binding.pry 
+  end  
+  
+  def deposit(n)
+    self.balance = balance +  n 
+    #binding.pry 
+  end 
+  
+  def display_balance
+    "Your balance is $#{self.balance}." 
+    #binding.pry
+  end 
+  
+  def valid?
+    if self.balance > 0 && status == 'open'
+      true 
+    else 
+      false
+    end 
+  end 
+  
+  def close_account 
+    self.status = "closed"
+  end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, status = 'pending', amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end 
+  
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      true
+    else
+      false 
+    end 
+    #binding.pry  
+  end
+  
+  def execute_transaction
+    
+  end 
 end
